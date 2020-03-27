74e74d1c4272bd480fcb21e8095e0d3902c24e9d
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2020-03-27 10:49:54 +0700

    Copyrights updated

--- src/main/java/org/twodividedbyzero/idea/findbugs/actions/AnalyzeModuleFilesIncludingTests.java
+++ src/main/java/org/twodividedbyzero/idea/findbugs/actions/AnalyzeModuleFilesIncludingTests.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright 2008-2019 Andre Pfeiler
+ * Copyright 2020 SpotBugs plugin contributors
+ *
+ * This file is part of IntelliJ SpotBugs plugin.
+ *
+ * IntelliJ SpotBugs plugin is free software: you can redistribute it 
+ * and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of 
+ * the License, or (at your option) any later version.
+ *
+ * IntelliJ SpotBugs plugin is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with IntelliJ SpotBugs plugin.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.twodividedbyzero.idea.findbugs.actions;
 
 public final class AnalyzeModuleFilesIncludingTests extends AnalyzeModuleFiles {
--- src/main/java/org/twodividedbyzero/idea/findbugs/gui/toolwindow/view/BugTreePanel.java
+++ src/main/java/org/twodividedbyzero/idea/findbugs/gui/toolwindow/view/BugTreePanel.java
@@ -1,20 +1,22 @@
 /*
  * Copyright 2008-2019 Andre Pfeiler
+ * Copyright 2020 SpotBugs plugin contributors
  *
- * This file is part of FindBugs-IDEA.
+ * This file is part of IntelliJ SpotBugs plugin.
  *
- * FindBugs-IDEA is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * IntelliJ SpotBugs plugin is free software: you can redistribute it 
+ * and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of 
+ * the License, or (at your option) any later version.
  *
- * FindBugs-IDEA is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * IntelliJ SpotBugs plugin is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with FindBugs-IDEA.  If not, see <http://www.gnu.org/licenses/>.
+ * along with IntelliJ SpotBugs plugin.
+ * If not, see <http://www.gnu.org/licenses/>.
  */
 package org.twodividedbyzero.idea.findbugs.gui.toolwindow.view;
 
