a109c503f4a769d8729f1d3bbbfebfd187f0cb5e
jqyp <jqy@protonmail.com>
2020-06-15 07:20:00 +0300

    Removed unused import

--- src/main/java/org/jetbrains/plugins/spotbugs/gui/editor/BugAnnotator.java
+++ src/main/java/org/jetbrains/plugins/spotbugs/gui/editor/BugAnnotator.java
@@ -30,7 +30,6 @@ import com.intellij.psi.PsiAnonymousClass;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.ui.JBColor;
 import edu.umd.cs.findbugs.BugInstance;
 import edu.umd.cs.findbugs.Detector;
