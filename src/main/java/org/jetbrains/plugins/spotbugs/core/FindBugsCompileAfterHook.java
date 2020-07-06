1d7913fcb2e5634542871e2f6a4e60641bd613b2
jqyp <jqy@protonmail.com>
2020-07-06 20:34:35 +0300

    Fix possible IllegalArgumentException. This closes #29

--- src/main/java/org/jetbrains/plugins/spotbugs/core/FindBugsCompileAfterHook.java
+++ src/main/java/org/jetbrains/plugins/spotbugs/core/FindBugsCompileAfterHook.java
@@ -276,7 +276,7 @@ public class FindBugsCompileAfterHook implements CompilationStatusListener, Proj
 
 		DelayedExecutor(@NotNull final Project project) {
 			_project = project;
-			_alarm = new Alarm(Alarm.ThreadToUse.POOLED_THREAD);
+			_alarm = new Alarm(Alarm.ThreadToUse.POOLED_THREAD, project);
 		}
 
 		void schedule(@NotNull final Set<VirtualFile> changed) {
